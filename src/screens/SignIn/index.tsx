f380d306462f7d37120cf0138fb4fe2b704f1832
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-02-10 00:56:55 -0300

    Componente de titulo heading native base

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
@@ -17,6 +17,12 @@ export function SignIn() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   );
