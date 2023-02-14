507867bc26252d2068e0572a4f91c85871d8a370
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-02-14 00:24:43 -0300

    Componente de input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import { Center, Heading, Image, Text, VStack } from "native-base";
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
@@ -23,6 +24,8 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
           Acesse sua conta
         </Heading>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" />
       </Center>
     </VStack>
   );
